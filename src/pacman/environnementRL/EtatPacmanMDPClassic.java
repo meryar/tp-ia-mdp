ecb86745fdeb3d923228d019099fabaa8d955356
Guédet Adrien <adrien.guedet@etu.univ-lyon1.fr>
2019-10-22 11:26:36 +0200

    new try for mdp

--- src/pacman/environnementRL/EtatPacmanMDPClassic.java
+++ src/pacman/environnementRL/EtatPacmanMDPClassic.java
@@ -15,15 +15,23 @@ import environnement.Etat;
 public class EtatPacmanMDPClassic implements Etat , Cloneable{
 
 
-	int x_pacman, y_pacman, x_ghost, y_ghost;
+	int x_pacman, y_pacman, distClosestDot;
+	boolean ghost_up, ghost_down, ghost_left, ghost_right;
+
 	//List<StateAgentPacman> ghosts;
 
 	public EtatPacmanMDPClassic(StateGamePacman _stategamepacman){
 
 	x_pacman = _stategamepacman.getPacmanState(0).getX();
 	y_pacman = _stategamepacman.getPacmanState(0).getY();
-	x_ghost = _stategamepacman.getGhostState(0).getX();
-	y_ghost = _stategamepacman.getGhostState(0).getY();
+
+	StateAgentPacman ghost = _stategamepacman.getGhostState(0);
+	ghost_left = ghost.getX() == x_pacman - 1 && ghost.getY() == y_pacman;
+	ghost_right = ghost.getX() == x_pacman + 1 && ghost.getY() == y_pacman;
+	ghost_up = ghost.getX() == x_pacman && ghost.getY() == y_pacman + 1;
+	ghost_down = ghost.getX() == x_pacman && ghost.getY() == y_pacman - 1;
+
+	distClosestDot =_stategamepacman.getClosestDot(_stategamepacman.getPacmanState(0));
 
 	/*ghosts = new ArrayList<>();
 	for (int i = 0; i<_stategamepacman.getNumberOfGhosts(); i++) {
@@ -38,15 +46,24 @@ public class EtatPacmanMDPClassic implements Etat , Cloneable{
 		/*for (int i = 0; i<ghosts.size(); i++) {
 			result += ghosts.get(i).getX()*ghosts.get(i).getY();
 		}*/
-		result += x_ghost * y_ghost;
+		if (ghost_left){ result += 1000;}
+		else if (ghost_right){ result += 1001;}
+		else if (ghost_down){ result += 1002;}
+		else if (ghost_up){ result += 1003;}
+		result += distClosestDot;
 
 		return result;
 	}
 	
 	@Override
 	public String toString() {
-		
-		return "";
+		String posGhost = " unknown";
+		if (ghost_left){ posGhost = " left";}
+		else if (ghost_right){ posGhost = " right";}
+		else if (ghost_up){ posGhost = " up";}
+		else if (ghost_down){ posGhost = " down";}
+
+		return " pacman[" + x_pacman + "," + y_pacman + "] and pos ghost" + posGhost + " and closest dot at " + distClosestDot ;
 	}
 	
 	
