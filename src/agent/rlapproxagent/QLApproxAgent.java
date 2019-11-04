386de3e7727f71b29204a955342577c491c7c626
Guédet Adrien <adrien.guedet@etu.univ-lyon1.fr>
2019-11-04 16:42:50 +0100

    agent qui marche jusqu'a preuve du contraire

--- src/agent/rlapproxagent/QLApproxAgent.java
+++ src/agent/rlapproxagent/QLApproxAgent.java
@@ -19,12 +19,17 @@ import environnement.Etat;
 public class QLApproxAgent extends QLearningAgent{
 
 	FeatureFunction featureFunction;
+	double[] theta;
 	
 	public QLApproxAgent(double alpha, double gamma, Environnement _env,FeatureFunction _featurefunction) {
 		super(alpha, gamma, _env);
 		//*** VOTRE CODE
 		featureFunction = _featurefunction;
 
+		theta = new double[_featurefunction.getFeatureNb()];
+		for (int i = 0; i < theta.length; i++){
+			theta[i] = 1;
+		}
 		
 	}
 
@@ -33,7 +38,12 @@ public class QLApproxAgent extends QLearningAgent{
 	public double getQValeur(Etat e, Action a) {
 		//*** VOTRE CODE
 		double[] features = featureFunction.getFeatures(e, a);
-		return features[0];
+		double res = 0;
+		for(int i = 0; i< featureFunction.getFeatureNb(); i++){
+			res += theta[i] * features[i];
+		}
+
+		return res;
 
 	}
 	
@@ -46,9 +56,23 @@ public class QLApproxAgent extends QLearningAgent{
 			System.out.println("QL: mise a jour poids pour etat \n"+e+" action "+a+" etat' \n"+esuivant+ " r "+reward);
 		}
        //inutile de verifier si e etat absorbant car dans runEpisode et threadepisode 
-		//arrete episode lq etat courant absorbant	
-		
+		//arrete episode lq etat courant absorbant
+
+
 		//*** VOTRE CODE
+		double Qvaleur = this.getQValeur(e,a);
+		double[] features = featureFunction.getFeatures(e, a);
+		double bestQValeur = Integer.MIN_VALUE;
+
+		for(Action action: this.getActionsLegales(esuivant)){
+			if (this.getQValeur(esuivant,action) > bestQValeur){
+				bestQValeur = this.getQValeur(esuivant,action);
+			}
+		}
+
+		for(int i = 0; i< featureFunction.getFeatureNb(); i++){
+			theta[i] += this.alpha * features[i] * (reward + this.gamma * bestQValeur - Qvaleur);
+		}
 		
 		
 	}
@@ -59,6 +83,10 @@ public class QLApproxAgent extends QLearningAgent{
 		this.qvaleurs.clear();
 	
 		//*** VOTRE CODE
+
+		for (int i = 0; i < theta.length; i++){
+			theta[i] = 1;
+		}
 		
 		this.episodeNb =0;
 		this.notifyObs();
